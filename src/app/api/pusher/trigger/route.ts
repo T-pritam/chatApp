89e2c97f4f15cf19f468b9a8535d9245aa7ba2f3
T-pritam <pritamrao38@gmail.com>
2024-10-11 20:00:32 +0530

    bugs fixed in Authentication

--- src/app/api/pusher/trigger/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import pusher from '@/lib/pusher';
-
-export async function POST(req: NextRequest) {
-  const { senderId, text } = await req.json();
-
-  await pusher.trigger('chat-room', 'new-message', { senderId, text });
-  return NextResponse.json({ status: 'Message sent' });
-}
