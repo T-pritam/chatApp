ef2204908643821183eff58890a36868d3a43add
T-pritam <pritamrao38@gmail.com>
2024-10-13 21:44:08 +0530

    Bugs fixed

--- src/features/counter/store.ts
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterSlice  from './counterStore';
-import { count } from 'console';
-
-export const store = configureStore({
-    reducer : counterSlice
-}) 
